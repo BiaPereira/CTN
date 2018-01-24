18443655f5d7feba09dd820c834fb0bd60de5382
BiaPereira <beatrizcppereira@gmail.com>
2018-01-24 17:19:23 +0000

    Ler ficheiros

--- ITN.py
+++ ITN.py
@@ -1,20 +1,45 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+import ezodf
 
 #Opens file
-filename = input('Name of the file: ')
-file = open(filename)
-print('\n')
+#filename = input('Name of the file: ')
+file = open('F2LiCl_001.txt')
+print("open")
 fileR = file.read()
-print(fileR)
-
-#str to array
-
+#print (fileR)
+print (type(fileR))
 
 
-#data = list(fileR)
-#print (data)
-#shape=data.shape
+
+g = fileR.split(' ',-1)
+print(g)
+#f = fileR.split('\n', -1)
+#print (f)
+leng = len(g)
+print (leng)
 #print (shape)
-#Max = np.max(fileR)
-#print (Max)
+#print(g)
+i=0
+x = []
+y = []
+for i in range(0,leng-1,4):
+
+    print ('Novos valores')
+    print(g[i + 1])
+    print(g[i+3])
+    x.append(g[i + 1])
+    y.append(g[i+3])
+print (x)
+maxx = max(x)
+print (len(x))
+print (y)
+maxy = max(y)
+print (len(y))
+print (type(x))
+
+plt.plot(x,y)
+#plt.axis([0, 160, 0, 0.03])
+plt.show()
+
