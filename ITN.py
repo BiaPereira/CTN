a9ff69ebc8c0da82f47efe1b3aaf54fa86c3c18b
BiaPereira <beatrizcppereira@gmail.com>
2018-01-26 14:43:38 +0000

    gráfico gauss

--- ITN.py
+++ ITN.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from bisect import bisect
 from matplotlib.backends.backend_pdf import PdfPages
 import math
 from scipy.optimize import curve_fit
@@ -14,48 +15,21 @@ from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 
 
 #Opens file
-#filename = input('Name of the file: ')
-file = open('F2LiCl_001.txt')
+filename = input('Name of the file: ')
+tam = len(filename)
+if filename[tam-3:tam] == 'txt' :
+    file = open(filename)
+if filename[tam-3:tam] == 'odf' :
+    base = os.path.splitext(filename)[0]
+    os.rename(filename, base + ".txt")
+    file = open(filename)
+else:
+    print('Ficheiro não suportado')
+
+
 fileR = file.read()
-#print (fileR)
 
-
-g = fileR.split(' ',-1)
-leng = len(g)
-
-#Two list
-i=0
-x = []
-y = []
-for i in range(0,leng-1,4):
-
-    x.append(float(g[i + 1]))
-    y.append(float(g[i+3]))
-
-print('Maximo de x', max(x))
-print('Maximo de y', max(y))
-
-
-# Plot
-plt.plot(x,y)
-plt.grid()
-
-# legend
-plt.plot(y, label="Experimental")
-#plt.plot(gauss, label="Gaussiana") # legenda para a gaussiana
-plt.legend(bbox_to_anchor=(0.8, 1), loc=2, borderaxespad=0.)
-
-# Axes
-plt.ylabel('Intensidade')
-plt.xlabel('Canal')
-#plt.axhline(y=0) linha horizontal
-#plt.axvline(x=0) linha vertical
-plt.yscale('log', nonposy='clip')
-plt.show()
-
-lim_min=int(input('Lower limit: '))
-lim_max=int(input('Upper limit: ' ))
-
+#function
 def zoom_gráfico (minimo, maximo):
     lmin = x.index(minimo)
     lmax = x.index(maximo)
@@ -70,7 +44,58 @@ def zoom_gráfico (minimo, maximo):
     plt.xlabel('Canal')
     plt.show()
 
+#Gauss
+def gaussiana (A, y0,index_x, FWHM, x):
+    gauss = y0 + (A/((FWHM/(np.log(4))**(1/2))))*np.exp(-2*((x-index_x)**2)/((FWHM/(np.log(4))**(1/2))**2))
+    return gauss
 
+#split
+g = fileR.split(' ',-1)
+leng = len(g)
+
+#Two list
+i=0
+x = []
+y = []
+for i in range(0,leng-1,4):
+
+    x.append(float(g[i + 1]))
+    y.append(float(g[i+3]))
+
+#Max and min vector x and y
+max_x = max(x)
+min_x = min(x)
+max_y = max(y)
+min_y = min(y)
+print('Maximo de x', max_x)
+print('Maximo de y', max_y)
+
+
+# Plot
+plt.plot(x,y)
+plt.grid()
+
+# legend
+plt.plot(y, label="Experimental")
+plt.legend(bbox_to_anchor=(0.8, 1), loc=2, borderaxespad=0.)
+
+# Axes
+plt.ylabel('Intensidade')
+plt.xlabel('Canal')
+plt.xlim(min_x,max_x)
+#plt.ylim(min_y,max_y)
+#plt.axhline(y=0) linha horizontal
+#plt.axvline(x=0) linha vertical
+plt.yscale('log', nonposy='clip')
+plt.show()
+
+
+#---------------------New plot
+
+
+# input limit plot
+lim_min=int(input('Lower limit: '))
+lim_max=int(input('Upper limit: ' ))
 
 zoom_gráfico(lim_min,lim_max)
 resp = input('Gráfico Correto? Sim/Não: ')
@@ -80,62 +105,62 @@ while resp == 'Não':
     zoom_gráfico(lim_min,lim_max)
     resp = input('Gráfico Correto? Sim/Não: ')
 
-#print ('entre que valores está o pico máximo1?')
-    #min1 = int(input('Lower limit: '))
-    #max1 = int(input('Upper limit: '))
-min1 = 2620
-max1 = 2720
-xx = x[min1:max1 + 1]
-yy = y[min1:max1 + 1]
-print (xx)
-print ('veotr yy',yy)
-maxy = max(yy)
-print ('maxy',maxy)
-FWHMy = maxy/2
-print ('FWHMy',FWHMy)
-print ('yy.index(maxy)',yy.index(maxy))
-print ('yy.index(FWHMy)', yy.index(FWHMy))
+print ('Entre que valores está o pico máximo1?')
+min_xx = int(input('Lower limit: '))
+max_xx = int(input('Upper limit: '))
 
-index_max_y = yy.index(maxy) + min1
-index_FWHM_y = yy.index(FWHMy) + min1
+# New vector x and y (shorter)
+xx = x[min_xx:max_xx + 1]
+yy = y[min_xx :max_xx + 1]
 
-FWHMx = (abs(index_max_y-index_FWHM_y)*2)
+
+max_yy = max(yy)#max_counts
+FWHMyy = max_yy/2 #number of FWHM yy
+print ('FWHMyy',FWHMyy)
+
+#In case that FWHM does not appear in yy
+if FWHMyy in yy:
+    index_FWHM_y = yy.index(FWHMyy) + min_xx
+
+else:
+    y2 = sorted(yy) #order
+    print (y2)
+    index_FWHM_y2 = bisect(y2,FWHMyy)
+    print ('index_FWHM_y2',index_FWHM_y2)
+    number = y2[index_FWHM_y2]
+    print (number)
+    index_FWHM_y = yy.index(number) + min_xx
+
+print ('yy.index(maxy)',yy.index(max_yy))
+print ('yy.index(FWHMy)', index_FWHM_y)
+index_max_y = yy.index(max_yy) + min_xx
+
+FWHM_xx = (abs(index_max_y-index_FWHM_y)*2)
 FWHM2 = (abs(index_max_y-index_FWHM_y))
 
-print ('FWHMX' ,FWHMx)
+print ('FWHMX' ,FWHM_xx)
 print ('index_max_y',index_max_y)
 print ('index_FWHM_y',index_FWHM_y)
 
-inicio = index_max_y - min1 - FWHM2
+#Area
+inicio = index_max_y - min_xx - FWHM2
 print(type(inicio))
-fim = inicio + FWHMx
+fim = inicio + FWHM_xx
 print(type(fim))
 area = 0
 j = inicio
-print (inicio)
-print (fim)
+print ('inicio',inicio)
+print ('fim', fim)
 for j in range(inicio, fim,1):
     print (j)
     area = (area + yy[j])
-    print ('area:', area)
+    #print ('area:', area)
 
 xx=np.array(xx, dtype=float)
 
-def gaussiana (A, y0,index_x, FWHM, x):
-    gauss = y0 + (A/((FWHM/(np.log(4))**(1/2))))*np.exp(-2*((x-index_x)**2)/((FWHM/(np.log(4))**(1/2))**2))
-    return gauss
-print (gaussiana(area, 10,yy.index(maxy), FWHMx, xx))
-k = gaussiana(area, 10,index_max_y, FWHMx, xx)
-print (type (k))
-print (type (xx))
-print (xx)
+#print (gaussiana(area, 10,yy.index(max_yy), FWHM_xx, xx))
 
-plt.show()
+# New plot
+k = gaussiana(area, 10,index_max_y, FWHM_xx, xx)
 plt.plot(xx,k, xx,yy)
-pp = PdfPages('image.pdf')
-plt.savefig(pp, format='pdf')
-pp.savefig()
-
-
-
-
+plt.show()
